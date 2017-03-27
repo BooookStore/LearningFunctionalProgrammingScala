0f101a1e55859738ed31a0d85c14f6440ad5420e
BooookStore <booookstore@icloud.com>
2017-03-27 10:41:40 +0900

    [Add] ExceptionとOptionの違いに関するコードを作成

---
+++ errorhandling/failingFn.scala
@@ -0,0 +1,27 @@
+/**
+参照通過性がないコード
+*/
+
+def failingFn1(i: Int): Int = {
+  val y: Int = throw new Exception("fail!") // 例外をスロー
+  try {
+    val x = 42 + 5
+    x + y
+  } catch {
+    case e: Exception => 43
+  }
+}
+
+def failingFn2(i: Int): Int = {
+  try {
+    val x = 42 + 5
+    x + ((throw new Exception("fail!")): Int) // 例外をスロー
+  } catch {
+    case e: Exception => 43
+  }
+}
+
+def mean(xs: Seq[Double]): Double =
+  if(xs.isEmpty)
+    throw new ArithmeticException("mean of empty list!")
+  else xs.sum / xs.length
