14bfeda68b70ea5b1a5fc74236b8a9eab9433070
BooookStore <booookstore@icloud.com>
2017-03-27 12:43:16 +0900

    [Add] Optionに便利な関数を定義

--- errorhandling/Option.scala
+++ errorhandling/Option.scala
@@ -1,7 +1,35 @@
-sealed trait Option[+A]
+sealed trait Option[+A] {
+
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
+
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    this map (Some(_)) getOrElse ob
+
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
+
+}
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
-def mean(xs: Seq[Double]): Option[Double] =
-  if(xs.isEmpty) None
-  else Some(xs.sum / xs.length)
+object Option {
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if(xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x -m,2))))
+
+}
