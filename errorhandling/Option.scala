02cd43d1e26af3283b48cca56712b74982e17b46
BooookStore <booookstore@icloud.com>
2017-03-28 11:18:17 +0900

    [Add] Optionを持つリストから、Optionの持つ値のリストを生成する関数を定義

--- errorhandling/Option.scala
+++ errorhandling/Option.scala
@@ -1,10 +1,22 @@
 sealed trait Option[+A] {
 
+  /**
+  Optionの持つ値を別の値へと変更します。
+  OptionがNoneの場合、変更は実施されずNoneが返されます。
+  */
   def map[B](f: A => B): Option[B] = this match {
     case None => None
     case Some(a) => Some(f(a))
   }
 
+  /**
+  Optionの持つ値を別の値へと変更します。
+  OptionがNoneの場合、変更は実施されずNoneが返されます。
+  
+  mapと異なる点は、持っている値から新しいOptionを生成する点です。
+  よって、持っている値の変更結果がOptionになる関数を引数として与える場合、Optionの持つ値が
+  Optionになってしまうという２重構造のOptionの生成を防ぐことが可能です。
+  */
   def flatMap[B](f: A => Option[B]): Option[B] =
     map(f) getOrElse None
 
@@ -31,5 +43,17 @@ object Option {
 
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs) flatMap (m => mean(xs.map(x => math.pow(x -m,2))))
+
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
+
+  // ２つのOptionが持つ値より、計算を行い、計算結果をOptionとして返します。
+  def map2[A,B,C](a: Option[A], b:Option[B])(f: (A,B) => C): Option[C] =
+    a flatMap (aa => b map (bb => f(aa,bb)))
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+    }
 
 }
