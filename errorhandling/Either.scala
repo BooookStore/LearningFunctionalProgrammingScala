1fe727aa39cd12fcffe611891d00b5875c31e40f
BooookStore <booookstore@icloud.com>
2017-04-15 13:13:35 +0900

    [Add] traverse,sequence関数を追加

--- errorhandling/Either.scala
+++ errorhandling/Either.scala
@@ -11,20 +11,18 @@ sealed trait Either[+E, +A] {
   }
 
   def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
-    case Left(_) => Left(_)
+    case Left(a) => Left(a)
     case Right(a) => f(a)
   }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = this match {
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
     for {
       aa <- this
       bb <- b
     } yield f(aa,bb)
-  }
-
-
 
 }
+
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
@@ -38,5 +36,24 @@ object Either {
       Left("mean of empty list.")
     else
       Right(xs.sum / xs.length)
+
+  /*
+  リストに格納された値を特定の値へと変更する。
+  リストに格納されているすべての値の変換が成功すれば、変換後の値をリストとして保持するRight型を
+  返す。
+  変換に失敗した際にはエラーメッセージを持つLeft型を返す。
+  */
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es match {
+      case Nil => Right(Nil)
+
+      // 現在のリストのヘッド値を関数fによって変換
+      // 後続のリストの値を再帰呼び出しにより変換
+      // これら２つの値をmap2関数によってリスト化
+      case h::t => (f(h) map2 traverse(t)(f))(_ :: _)
+    }
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(x => x)
 
 }
