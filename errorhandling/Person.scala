d627865f07d34497c9036ba9fd7c3a56e152c034
bookstore <booookstore@icloud.com>
2017-05-13 10:01:50 +0900

    [Fix] errorhandlingプロジェクトの不具合を修正

--- errorhandling/Person.scala
+++ errorhandling/Person.scala
@@ -1,16 +1,18 @@
 import Either._
 
-case class Person(name: Name, age: Age)
+case class Person(name: Name, age: Age) {
+
+  def mkName(name: String): Either[String, Name] =
+    if(name == "" || name == null) Left("Name is empty.")
+    else Right(new Name(name))
+
+  def mkAge(age: Int): Either[String, Age] =
+    if (age < 0) Left("Age is out of range.")
+    else Right(new Age(age))
+
+  def mkPerson(name: String, age: Int): Either[String, Person] =
+    mkName(name).map2(mkAge(age))(Person(_,_))
+    
+}
 sealed class Name(val value: String)
 sealed class Age(val value: Int)
-
-def mkName(name: String): Either[String, Name] =
-  if(name == "" || name == null) Left("Name is empty.")
-  else Right(new Name(name))
-
-def mkAge(age: Int): Either[String, Age] =
-  if (age < 0) Left("Age is out of range.")
-  else Right(new Age(age))
-
-def mkPerson(name: String, age: Int): Either[String, Person] =
-  mkName(name).map2(mkAge(age))(Person(_,_))
