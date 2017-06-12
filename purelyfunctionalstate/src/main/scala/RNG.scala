06d030031b1f9c4cfa5b9e7ab7a0d6a842ba5f7f
bookstore <booookstore@icloud.com>
2017-06-12 21:54:29 +0900

    [Add] _map,_map2を作成

--- purelyfunctionalstate/src/main/scala/RNG.scala
+++ purelyfunctionalstate/src/main/scala/RNG.scala
@@ -218,4 +218,15 @@ object RandomUtil {
     }
   }
 
+  /**
+   * flatMapを使用し、mapを実装。シンプルで、わかりやすい実装になった。
+   */
+  def _map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  /**
+   * flatMap と _map を使用して map2 を実装。シンプルで、わかりやすい実装になった。
+   */
+  def _map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => _map(rb)(b => f(a, b)))
 }
