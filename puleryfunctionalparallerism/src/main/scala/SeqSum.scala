860f0b86bf5691403c86316f0e38b0bef133bec2
bookstore <booookstore@icloud.com>
2017-06-26 21:20:01 +0900

    [Comment] コメントを追記

--- puleryfunctionalparallerism/src/main/scala/SeqSum.scala
+++ puleryfunctionalparallerism/src/main/scala/SeqSum.scala
@@ -7,12 +7,15 @@ object SeqSum {
     ints.foldLeft(0)((a,b) => a + b)
 
   // 実装その２
+  // IndexedSeqはスタンダードライブラリの一つのクラス。
+  // Vectorのような、ランダムアクセスリストのスーパークラスである。
+  // リストとは異なり、効率的な２分割関数(splitAt)を提供してくれる。
   def sum_second(ints: IndexedSeq[Int]): Int =
     if(ints.size <= 1)
-      ints.headOption getOrElse 0
+      ints.headOption getOrElse 0 // headOptionはcollectionすべてが実装している。
     else {
-      val (l,r) = ints.splitAt(ints.length/2)
-      sum(l) + sum(r)
+      val (l,r) = ints.splitAt(ints.length/2) // リストを２つに分割する。
+      sum(l) + sum(r) // ２分割したリストから再帰的に関数を呼び出し、計算結果を返す。
     }
 
 }
