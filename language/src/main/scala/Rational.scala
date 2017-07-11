ac381d0dfc0234dfc876e388231ceeb3b47a1468
bookstore <booookstore@icloud.com>
2017-07-11 21:42:20 +0900

    [Add] Rationalクラスに対して変数フィールドを追加し、コンストラクタ引数で初期化
    - 変数フィールドをしようしてtoString,add関数を書き換え

--- language/src/main/scala/Rational.scala
+++ language/src/main/scala/Rational.scala
@@ -10,6 +10,19 @@ class Rational(n: Int, d:Int) {
   /** 分母が０出ない場合において、オブジェクトを生成 */
   require(d != 0)
 
+  /** 分子 */
+  val number: Int = n
+
+  /** 分母 */
+  val denom: Int = d
+
   /** 現在持っている分子、分母の値を理解しやすい形で出力する */
-  override def toString = n + "/" + d
+  override def toString = number + "/" + denom
+
+  /** 分数同士の足し算を行い、結果を返す */
+  def add(that: Rational): Rational =
+    new Rational(
+      number * that.denom + that.number * denom,
+      denom * that.denom
+    )
 }
